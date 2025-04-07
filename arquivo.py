30a8bec774e0587305be216a92a0f190f99efe95
Sihfcruz <simeia.ruz@aluno.senai.br>
2025-04-07 14:22:27 -0300

    novo
    branch com a resposta completa e correta

--- arquivo.py
+++ arquivo.py
@@ -1,3 +1,9 @@
+soma=0
+quant=0
+print("me diga os valores, e te direi a soma e a média de tais")
+print("caso queira encerrar o programa digite um numero negativo")
+while True:
+    valor=float(input("digite o valor"))
 if valor<0:
         print("encerrado")
         break

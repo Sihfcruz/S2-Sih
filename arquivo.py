d5a0c9779c3c7b6c636bdc702c41440929a46cbd
Sihfcruz <simeia.ruz@aluno.senai.br>
2025-04-07 14:20:50 -0300

    errada
    branch com resposta errada

--- arquivo.py
+++ arquivo.py
@@ -4,3 +4,10 @@ print("me diga os valores, e te direi a soma e a média de tais")
 print("caso queira encerrar o programa digite um numero negativo")
 while True:
     valor=float(input("digite o valor"))
+if valor=55:
+        print("eliminado")
+        break
+print("voce foi eliminado")
+print("voce foi aprovado")
+
+

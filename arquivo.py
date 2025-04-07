6e7d619d3e07f6a4ea765940c7548ffe465667b3
Sihfcruz <simeia.ruz@aluno.senai.br>
2025-04-07 14:13:15 -0300

    main
    código do python que está incompleto (para minha dupla completar)

---
+++ arquivo.py
@@ -0,0 +1,6 @@
+soma=0
+quant=0
+print("me diga os valores, e te direi a soma e a média de tais")
+print("caso queira encerrar o programa digite um numero negativo")
+while True:
+    valor=float(input("digite o valor"))

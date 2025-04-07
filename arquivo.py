339a4b2a239cf4f329e1f3e96c9c731e5ac1a657
Sihfcruz <simeia.ruz@aluno.senai.br>
2025-04-07 14:16:34 -0300

    certo
    restante do código (certo)

--- arquivo.py
+++ arquivo.py
@@ -1,6 +1,7 @@
-soma=0
-quant=0
-print("me diga os valores, e te direi a soma e a média de tais")
-print("caso queira encerrar o programa digite um numero negativo")
-while True:
-    valor=float(input("digite o valor"))
+if valor<0:
+        print("encerrado")
+        break
+    soma=soma+valor
+    quant=quant+1
+print("a soma dos valores é de:",soma)
+print("a média dos valores é de:",soma/quant)
